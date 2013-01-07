eca844fde4a18f6c88602773a93400a94c2d1bb6
punk <pgotsr@punkscan1dot2-dev.(none)>
2013-01-07 03:16:38 +0000

    resolved encoding issues with crawldb parser

--- punkscan/crawl_db_parser/crawldb_parser.py
+++ punkscan/crawl_db_parser/crawldb_parser.py
@@ -14,6 +14,7 @@ config_parser.read(os.path.join(punkscan_base,'punkscan_configs', 'punkscan_conf
 HADOOP_HOME=config_parser.get('directories','HADOOP_HOME')
 NUTCH_HOME=config_parser.get('directories','NUTCH_HOME')
 NUTCH_RUNTIME_DEP = os.path.join(NUTCH_HOME, "runtime", "deploy")
+import re
 
 class CrawlDBParser:
 
@@ -46,22 +47,54 @@ class CrawlDBParser:
 
         hadooper.Hadooper().copyToLocal('punkscan_crawl_dump', os.path.join(NUTCH_RUNTIME_DEP, 'punkscan_crawl_dump'))
 
+
+    def __skip_ascii_null(self, csv_data):
+
+        for line in csv_data:
+
+            try:
+
+                yield line
+
+            except:
+
+                pass
+
     def crawl_db_url_generator(self):
-        '''Generator for the urls from the crawldb  '''
+        '''Generator for the urls from the crawldb'''
 
         crawl_db_files = glob.glob(os.path.join(NUTCH_RUNTIME_DEP, "punkscan_crawl_dump", "*"))
-
+        trouble_list = []
         for crawl_db_file in crawl_db_files:
-	    crawl_db_csv = open(crawl_db_file, "rb")
-	    crawl_db_read = csv.DictReader(crawl_db_csv, delimiter = ";", quotechar='"')
 
-	    for row in crawl_db_read:
-	        yield row["Url"]
-		
+            crawl_db = open(crawl_db_file, "r")
+            iterfile = iter(crawl_db)
+            next(iterfile)
+
+            for line in iterfile:
+
+                #we use regular expressions because of the encoding deficiencies in the csv module
+                m = re.match(r'\"(.*)\";(.*);(.*);(.*);(.*);(.*);(.*);(.*);(.*);', line)
+
+                if m:
+                    result = m.group(1)
+                else:
+                    trouble_list.append(line)
+
+                yield result
+        
+        print "Had trouble reading the following entries in the crawldb:"
+
+        for line in trouble_list:
+
+            print line
+        
+        print "Skipped %s URLs because we had trouble reading them: " % str(len(trouble_list))
+
 if __name__ == "__main__":
 
     x = CrawlDBParser()
-    x.dump_crawl_db()
-    x.get_crawl_db_dump()
+#    x.dump_crawl_db()
+#    x.get_crawl_db_dump()
     for whatever in x.crawl_db_url_generator():
         print whatever
