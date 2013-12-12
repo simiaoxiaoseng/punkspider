d63eb91039ab049640127e0bc3194babb2a7e891
punk <punk@punk-kali.punk-kali.hyperiongray.com>
2013-12-12 14:19:23 -0500

    punkcrawler

--- punkscan/punkcrawler.py
+++ punkscan/punkcrawler.py
@@ -25,17 +25,11 @@ sys.path.append(os.path.join(punkscan_base, "config_scripts"))
 sys.path.append(os.path.join(punkscan_base, "crawl_db_parser"))
 sys.path.append(os.path.join(punkscan_base, "crawler"))
 sys.path.append(os.path.join(punkscan_base, "punk_solr"))
-import reconfig_nutch
-import crawldb_parser
-import hadooper
-import nutch
 import punkscan_solr
 from ConfigParser import SafeConfigParser
 
 config_parser = SafeConfigParser()
 config_parser.read(os.path.join(punkscan_base,'punkscan_configs', 'punkscan_config.cfg'))
-HADOOP_HOME=config_parser.get('directories','HADOOP_HOME')
-NUTCH_HOME=config_parser.get('directories','NUTCH_HOME')
 
 
 def configure_punkscan():
